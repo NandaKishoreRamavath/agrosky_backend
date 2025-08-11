b7e410d14a78676a16c20a695cb8accfc4269e22
Nanda Kishore <98969700+NandaKishoreRamavath@users.noreply.github.com>
2025-08-12 01:28:54 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors(
   {
-    origin: "http://localhost:3000"
+    origin: "https://agrosky-frontend.onrender.com"
   }
 ));
 app.use(rateLimiter);
